4b457f944c5cc55319464b99b4568b4d4fb181d7
mickey_zzc <mickey_zzc@163.com>
2026-05-17 17:21:04 +0800

    feat(ui): add archives management page

--- web/src/components/Header.svelte
+++ web/src/components/Header.svelte
@@ -60,6 +60,7 @@
     { href: '#/dashboard', labelKey: 'nav.dashboard', route: '/dashboard' },
     { href: '#/recordings', labelKey: 'nav.recordings', route: '/recordings' },
     { href: '#/cameras', labelKey: 'nav.cameras', route: '/cameras' },
+    { href: '#/archives', labelKey: 'nav.archives', route: '/archives' },
     { href: '#/plugins', labelKey: 'nav.plugins', route: '/plugins' },
     { href: '#/stats', labelKey: 'nav.stats', route: '/stats' },
     { href: '#/settings', labelKey: 'nav.settings', route: '/settings' },
