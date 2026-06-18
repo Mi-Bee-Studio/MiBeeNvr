acf8cf4b2c347ac5b65a982686e1bd2c0799940e
mickeyzzc <mickeyzzc@users.noreply.github.com>
2026-06-18 15:58:16 +0800

    test: increase wsstream read timeout for CI race detector reliability

--- internal/wsstream/manager_test.go
+++ internal/wsstream/manager_test.go
@@ -50,7 +50,7 @@ func dialWS(t *testing.T, url string) *websocket.Conn {
 
 func readMessage(t *testing.T, conn *websocket.Conn) ([]byte, error) {
 	t.Helper()
-	conn.SetReadDeadline(time.Now().Add(3 * time.Second))
+	conn.SetReadDeadline(time.Now().Add(10 * time.Second)) // generous for -race on CI
 	_, msg, err := conn.ReadMessage()
 	return msg, err
 }
